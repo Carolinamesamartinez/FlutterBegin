58746e7aecfc9005779cc87f7eb7cbba26e612af
c.mesa.martinez <carolinamesamartinez1@gmail.com>
2023-05-23 10:19:50 +0200

    writing notes to cloud firestore pt2

--- lib/services/cloud/firebase_cloud_storage.dart
+++ lib/services/cloud/firebase_cloud_storage.dart
@@ -1,4 +1,54 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:secondflutter/services/cloud/cloud_note.dart';
+import 'package:secondflutter/services/cloud/cloud_storage_constants.dart';
+import 'package:secondflutter/services/cloud/cloud_storage_exceptions.dart';
+
 class FirebaseCloudStorage {
+  //acces to notes collection in cloud firestire
+  final notes = FirebaseFirestore.instance.collection('notes');
+
+  Future<void> updateNote(
+      {required String documentId, required String text}) async {
+    try {
+      await notes.doc(documentId).update({textFieldName: text});
+    } catch (e) {
+      throw CouldNotUpdateNote();
+    }
+  }
+
+  Future<void> deleteNote({required String documentId}) async {
+    try {
+      await notes.doc(documentId).delete();
+    } catch (e) {
+      throw CouldNotDeleteNote();
+    }
+  }
+
+  Stream<Iterable<CloudNote>> allNotes({required String ownerUserId}) =>
+      notes.snapshots().map(((event) => event.docs
+          .map((doc) => CloudNote.fromSnapshot(doc))
+          .where((note) => note.ownerUserId == ownerUserId)));
+
+  Future<Iterable<CloudNote>> getNotes({required String ownewUserId}) async {
+    try {
+      return await notes
+          .where(ownerUserIdFieldName, isEqualTo: ownewUserId)
+          .get()
+          .then((value) => value.docs.map((doc) {
+                return CloudNote(
+                    documentId: doc.id,
+                    ownerUserId: doc.data()[ownerUserIdFieldName] as String,
+                    text: doc.data()[textFieldName] as String);
+              }));
+    } catch (e) {
+      throw CouldNotGetAllNotes();
+    }
+  }
+
+  void createNewNote({required String ownewUserId}) async {
+    await notes.add({ownerUserIdFieldName: ownewUserId, textFieldName: ''});
+  }
+
   static final FirebaseCloudStorage _shared =
       FirebaseCloudStorage._sharedInstance();
   FirebaseCloudStorage._sharedInstance();
